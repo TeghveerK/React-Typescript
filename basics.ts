d25f49f74e31de028a515d94e133c48b750f2e7f
TeghveerK <tkooner@uci.edu>
2023-03-14 17:25:26 -0700

    Exloring Base Types

---
+++ basics.ts
@@ -0,0 +1,14 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, object
+// Function types, parameters
+
+// Primitives
+
+let age: numbe;
+age = 12;
+
+let userName: strin;
+userName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;
