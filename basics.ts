f5e9e90f323286ba2d0f56336142fa65a6d8da8e
TeghveerK <tkooner@uci.edu>
2023-03-16 15:44:34 -0700

    Type Inference

--- basics.ts
+++ basics.ts
@@ -4,11 +4,38 @@
 
 // Primitives
 
-let age: numbe;
+let age: number;
 age = 12;
 
-let userName: strin;
+let userName: string;
 userName = "Max";
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+
+let course = "React - The Complete Guide";
+
+//course = 12341;
